b506415ade3073e8fbc904ac68c48e7c711c8ab2
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-05-14 21:47:22 +0300

    asdasdas

---
+++ B21 Ex03 Yaniv 302374921 Itamar 206024796/Ex03.ConsoleUI/ConsoleUI.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using Ex03.GarageLogic;
+
+namespace Ex03.ConsoleUI
+{
+    class ConsoleUI
+    {
+        public string MenuString { get; set; }
+        public bool IsRunning { get; set; }
+        public Garage Garage { get; set; }
+
+        public ConsoleUI()
+        {
+            UpdateMenuString();
+        }
+
+        void UpdateMenuString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Garage Menu:");
+            stringBuilder.AppendLine("------------");
+            stringBuilder.AppendLine("1. Add a new vehicle");
+            stringBuilder.AppendLine("2. Display all license plates");
+            stringBuilder.AppendLine("3. Change vehicle state");
+            stringBuilder.AppendLine("4. Inflate car wheels to max");
+            stringBuilder.AppendLine("5. Fuel petrol car");
+            stringBuilder.AppendLine("6. Charge electric car");
+            stringBuilder.AppendLine("7. Display full car info");
+            stringBuilder.AppendLine("8. Exit");
+            MenuString = stringBuilder.ToString();
+        }
+
+        public void Run()
+        {
+            Console.ReadLine();
+
+            while (IsRunning)
+            {
+                Console.Clear();
+                DisplayMenu();
+                string stringInput = Console.ReadLine();
+                executeMenuSelection(stringInput);
+            }
+        }
+
+        private void executeMenuSelection(string i_StrChoice)
+        {
+            switch (i_StrChoice)
+            {
+                case "1":
+                    addVehicle();
+                    break;
+                case "2":
+                    DisplayAllLicensePlates();
+                    break;
+                case "3":
+                    ChangeVehicleState();
+                    break;
+                case "4":
+                    inflateVehicleWheelsToMax();
+                    break;
+                case "5":
+                    FuelPetrolVehicle();
+                    break;
+                case "6":
+                    chargeElectricVehicle();
+                    break;
+                case "7":
+                    DisplayFullCarInfo();
+                    break;
+                case "8":
+                    exit();
+                    break;
+            }
+        }
+
+        private void addVehicle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DisplayAllLicensePlates()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.AppendLine("Display All License Plates");
+            strBuilder.AppendLine("--------------------------");
+            strBuilder.AppendLine("Filter by tags:");
+            strBuilder.AppendLine("1. Repaired");
+            strBuilder.AppendLine("2. In Repair");
+            strBuilder.AppendLine("3. Paid");
+            strBuilder.AppendLine("");
+            Console.WriteLine(strBuilder);
+
+            string inputString = Console.ReadLine();
+            int choice = int.Parse(inputString);
+
+            List<string> licenseStrings;
+
+
+            switch(choice)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+            }
+            
+        }
+
+        private void ChangeVehicleState()
+        {
+            Console.WriteLine("Change Car Status");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Enter license number:");
+            string licenseNumberString = Console.ReadLine();
+
+            Console.WriteLine("Choosee state:");
+            Console.WriteLine("1. Repaired");
+            Console.WriteLine("2. In repair");
+            Console.WriteLine("3. Paid");
+            Console.WriteLine("Enter option: ");
+            string stateInputString = Console.ReadLine();
+            int choice = int.Parse(stateInputString);
+
+            List<string> licenseStrings;
+
+
+            switch (choice)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+            }
+        }
+
+        private void inflateVehicleWheelsToMax()
+        {
+            
+        }
+
+        private void FuelPetrolVehicle()
+        {
+            
+        }
+
+        private void chargeElectricVehicle()
+        {
+            
+        }
+
+        private void DisplayFullCarInfo()
+        {
+            
+        }
+
+        private void exit()
+        {
+            IsRunning = false;
+        }
+
+        void DisplayMenu()
+        {
+            Console.Write(MenuString);
+        }
+    }
+}
---
+++ B21 Ex03 Yaniv 302374921 Itamar 206024796/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ConsoleUI ui = new ConsoleUI();
+            ui.Run();
+        }
+    }
+}
