8484f58b26769a0d79c49bad217f93f4950bcbe1
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-05-14 16:47:47 +0300

    Changed again

--- B21 Ex03 Yaniv 302374921 Itamar 206024796/Ex03.GarageLogic/Garage.cs
+++ B21 Ex03 Yaniv 302374921 Itamar 206024796/Ex03.GarageLogic/Garage.cs
@@ -14,20 +14,26 @@
             for(int i = 0; i < numOfWheels; i++)
             {
                 Wheel currentWheelToInflate = CustomerBook.GetCustomer(i_LicenseNumber).Vehicle.Wheels[numOfWheels];
-                currentWheelToInflate.Inflate(currentWheelToInflate.CurrentAirPressure);
+                currentWheelToInflate.Inflate(currentWheelToInflate.MaxAirPressure);
             }
         }
 
         public void FuelPetrolVehicle(string i_LicenseNumber, eFuelType i_FuelType, int i_Quantity)
         {
             FuelVehicle vehicleToFuel = CustomerBook.GetCustomer(i_LicenseNumber).Vehicle as FuelVehicle;
-            vehicleToFuel.Refuel(i_Quantity, i_FuelType);
+            if(vehicleToFuel != null)
+            {
+                vehicleToFuel.Refuel(i_Quantity, i_FuelType);
+            }
         }
 
         public void ChargeElectricVehicle(string i_LicenseNumber, int i_ChargingTimeInMin)
         {
             ElectricityVehicle vehicleToCharge = CustomerBook.GetCustomer(i_LicenseNumber).Vehicle as ElectricityVehicle;
-            vehicleToCharge.ChargeBattery(i_ChargingTimeInMin);
+            if (vehicleToCharge != null)
+            {
+                vehicleToCharge.ChargeBattery(i_ChargingTimeInMin);
+            }
         }
 
     }
